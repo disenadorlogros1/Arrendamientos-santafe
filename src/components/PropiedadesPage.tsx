0a4b3b86c732213a64b7d9dcc06cbfbd99f65ef1
disenadorlogros1 <disenador1@logrosdigital.com>
2026-05-22 14:04:49 -0500

    Update PropiedadesPage.tsx

--- src/components/PropiedadesPage.tsx
+++ src/components/PropiedadesPage.tsx
@@ -164,7 +164,7 @@ export default function PropiedadesPage() {
         </p>
 
         {/* Property Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
           {filtered.map((property) => (
             <motion.div
               key={property.id}
