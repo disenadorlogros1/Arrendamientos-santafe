f5d80445a579b77eb83fe3e8e9d72e8bf2740f55
disenadorlogros1 <disenador1@logrosdigital.com>
2026-06-18 10:22:05 -0500

    Update PoliticasPage.tsx

--- src/components/PoliticasPage.tsx
+++ src/components/PoliticasPage.tsx
@@ -3,7 +3,7 @@
 export default function PoliticasPage() {
   return (
     <div className="min-h-screen bg-white">
-      <div className="bg-brand-dark py-12 md:py-16">
+      <div className="bg-brand-dark pb-12 md:pb-16" style={{ marginTop: '-43px', paddingTop: 'calc(43px + 48px)' }}>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl sm:text-4xl font-bold text-white">
             Política de tratamiento de datos personales
