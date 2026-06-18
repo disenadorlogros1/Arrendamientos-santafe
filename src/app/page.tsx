959443d30d523601440a82b72ec35f2a4e64eb7e
disenadorlogros1 <disenador1@logrosdigital.com>
2026-06-18 15:11:14 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Header currentPage={currentPage} onNavigate={handleNavigate} />
+      <Header currentPage={currentPage} onNavigate={handleNavigate} isHeroPage={currentPage === 'home'} />
 
       <main className="flex-1 relative" style={{ paddingTop: currentPage === 'home' ? 0 : '86px' }}>
         {currentPage === 'home' && <HomePage onNavigate={handleNavigate} />}
